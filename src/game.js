837eb78e340c958cd6cd629d283fcada17ce95a1
Andrey Kostin <andrey.kostin.111@mail.ru>
2024-12-30 21:18:17 +0300

    [CW2] Fix game process logic & add scenarios

--- src/game.js
+++ src/game.js
@@ -3,17 +3,23 @@
 (() => {
   const RPS = ['камень', 'ножницы', 'бумага'];
   const SCENARIO = {
-    greeting: '',
-    playingMove: '',
-    firstMove: '',
-    botMove: '',
-    playerMove: '',
-    resultMove: '',
-    summary: '',
-    input_error: '',
-    exit_confirm: '',
+    greeting: 'Добро пожаловать в игру "Марблы"!\nСначала сыграем в "Камень, ножницы, бумага", чтобы определить, кто начнёт первым.',
+    playingMove: 'Ваш ход в "Камень, ножницы, бумага".\nВведите: камень, ножницы или бумага:',
+    drawFirst: 'Ничья! Давайте попробуем ещё раз.',
+    isEven: 'Бот загадал число. Как думаете, оно чётное?\nOK - да, Отмена - нет',
+    player_positive: 'Поздравляем! Вы угадали.\nБот загадал число шариков: ""\nВаши шарики: ""\nШарики бота: ""',
+    player_negative: 'Увы, вы не угадали.\nБот загадал число шариков: ""\nВаши шарики: ""\nШарики бота: ""',
+    get_bet: 'Ваш ход!\nВаши шарики: ""\nШарики бота: ""\nВведите количество шариков, которое хотите поставить:',
+    bot_positive: 'Бот угадал ваше число. Шарики переходят к нему.\nВаши шарики: ""\nШарики бота: ""',
+    bot_negative: 'Бот ошибся. Вы получаете шарики.\nВаши шарики: ""\nШарики бота: ""',
+    now_move: 'Ход ""`a!',
+    summary: 'Игра завершена!\nПобедитель: ""\nШарики бота: ""\nШарики игрока: ""',
+    bye: 'Спасибо за игру! До встречи!',
+    again: 'Хотите сыграть ещё раз?',
+    input_error_rps: 'Ошибка ввода! Введите "камень", "ножницы" или "бумага".',
+    input_error_number: 'Ошибка ввода! Введите число от 1 до количества ваших шариков.',
+    exit_confirm: 'Вы уверены, что хотите выйти из игры?',
   };
-
   let marbles = {
     player: 5,
     bot: 5,
@@ -23,7 +29,7 @@
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
 
-    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+    return Number(Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled));
   };
 
   const normalizeInput = (input) => {
@@ -49,78 +55,100 @@
     const scenario = SCENARIO;
     const figures = RPS;
 
-    const botChoice = figures[getRandomIntInclusive(0, figures.length - 1)];
-    let userInput = prompt(scenario.playingMove);
-    let userChoice = normalizeInput(userInput);
+    let botChoice;
+    let userInput;
+    let userChoice;
+    let matchResult = 'DRAW';
 
-    while ((!userChoice || userInput.trim() === '') && userInput !== null) {
-      alert(scenario.input_error_rps);
+    while (matchResult === 'DRAW') {
+      botChoice = figures[getRandomIntInclusive(0, figures.length - 1)];
       userInput = prompt(scenario.playingMove);
       userChoice = normalizeInput(userInput);
-    }
 
-    if (userInput === null) {
-      const exitConfirm = confirm(scenario.exit_confirm);
-      if (exitConfirm) {
-        return 'EXIT';
+
+      while ((!userChoice || userInput.trim() === '') && userInput !== null) {
+        alert(scenario.input_error_rps);
+        userInput = prompt(scenario.playingMove);
+        userChoice = normalizeInput(userInput);
+      }
+
+      if (userInput === null) {
+        const exitConfirm = confirm(scenario.exit_confirm);
+        if (exitConfirm) {
+          return 'EXIT';
+        } else {
+          continue;
+        }
+      };
+
+      matchResult = checkResult(userChoice, botChoice);
+      if (matchResult === 'DRAW') {
+        alert(scenario.drawFirst);
       }
     };
-
-    const matchResult = checkResult(userChoice, botChoice);
-    if (matchResult === 'DRAW') {
-      alert(scenario.drawFirst);
-      playMove();
-    }
 
     return matchResult;
   };
 
-  const playMarbles = (first, scenario, rps) => {
+  const playMarbles = (first, scenario) => {
     const move = ['BOT', 'PLAYER'];
     let currentMove = move[first];
-    while (marbles.player > 0 || marbles.bot > 0) {
-      if (currentMove === 'BOT') { // бот загадывает количество, мы угадаываем
+    while (marbles.player > 0 && marbles.bot > 0) {
+      alert(scenario.now_move.replace('""', currentMove));
+      if (currentMove === 'BOT') {
         const botBet = getRandomIntInclusive(1, marbles.bot);
         const playerGuess = confirm(scenario.isEven);
-        if (playerGuess && botBet % 2 === 0) {
-          alert(scenario.player_positive);
+        if (playerGuess === (botBet % 2 === 0)) {
           marbles.player += botBet;
           marbles.bot -= botBet;
-        } else if (playerGuess && botBet % 2 !== 0) {
-          alert(scenario.player_negative);
+          alert(scenario.player_positive
+            .replace('""', botBet)
+            .replace('""', marbles.player)
+            .replace('""', marbles.bot));
+        } else {
           marbles.bot += botBet;
           marbles.player -= botBet;
+          alert(scenario.player_negative
+            .replace('""', botBet)
+            .replace('""', marbles.player)
+            .replace('""', marbles.bot));
         };
 
-        alert(scenario.now_player);
         currentMove = 'PLAYER';
-      } else if (currentMove === 'PLAYER') { // мы загадываем количество, бот угадывает
+      } else if (currentMove === 'PLAYER') {
         let playerBet;
-        while (!Number.isInteger(Number(playerBet)) || playerBet === '') {
-          playerBet = prompt(scenario.get_bet);
+        let playerNum = 0;
+        while (!Number.isInteger(Number(playerBet)) ||
+          playerBet === '' || playerNum < 1 || playerNum > marbles.player) {
+          playerBet = prompt(scenario.get_bet
+            .replace('""', marbles.player)
+            .replace('""', marbles.bot));
           if (playerBet === null) {
             return 'EXIT';
           };
-          const playerNum = Number(playerBet);
+          playerNum = Number(playerBet);
           const playerTrim = playerBet.trim();
           if (isNaN(playerNum) || !Number.isInteger(playerNum) ||
             playerTrim === '' || playerNum < 1 || playerNum > marbles.player) {
-            alert(scenario.input_error_numb);
+            alert(scenario.input_error_number);
             continue;
           }
         };
         const botGuess = getRandomIntInclusive(0, 1);
-        if (botGuess === playerBet % 2) {
-          alert(scenario.bot_positive);
-          marbles.bot += playerBet;
-          marbles.player -= playerBet;
+        if (botGuess === playerNum % 2) {
+          marbles.bot += playerNum;
+          marbles.player -= playerNum;
+          alert(scenario.bot_positive
+            .replace('""', marbles.player)
+            .replace('""', marbles.bot));
         } else {
-          alert(scenario.bot_negative);
-          marbles.player += playerBet;
-          marbles.bot -= playerBet;
+          marbles.player += playerNum;
+          marbles.bot -= playerNum;
+          alert(scenario.bot_negative
+            .replace('""', marbles.player)
+            .replace('""', marbles.bot));
         };
 
-        alert(scenario.now_bot);
         currentMove = 'BOT';
       }
     };
@@ -134,7 +162,8 @@
 
   const playGame = () => {
     const scenario = SCENARIO;
-    const figures = RPS;
+    marbles.bot = 5;
+    marbles.player = 5;
 
     alert(scenario.greeting);
     const firstMove = playMove();
@@ -142,9 +171,12 @@
       alert(scenario.bye);
       return;
     };
-    const winner,  = playMarbles(firstMove, scenario, figures);
+    const [winner, bot, player] = playMarbles(firstMove, scenario);
 
-    alert(scenario.summary);
+    alert(scenario.summary.replace('""', winner)
+      .replace('""', bot)
+      .replace('""', player));
+
     const again = confirm(scenario.again);
     if (again) {
       playGame();
@@ -154,5 +186,5 @@
     };
   };
 
-  window.rpc = playGame;
-});
+  window.marbles = playGame;
+})();
--- src/script.js
+++ src/script.js
@@ -1,5 +1,3 @@
 'use strict';
 
-const startGame = window.marbles();
-
-startGame();
+window.marbles();
